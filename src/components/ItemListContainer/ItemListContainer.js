3698fa295ea0fe5bc4677238e43cc9336ad78557
Andres Coria <andrescoria1984@gmail.com>
2022-09-20 23:05:50 -0300

    Desafio 4 counter

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 import Title from '../Title/Title'
 
 const ItemListContainer = () => {
 
   let greeting = 'Bienvenidos A Cerveceria Zaion'
 
+  const onAdd = (cantidad) => {
+    console.log(`agregaste ${cantidad} unidades`);
+  }
+
+
   return (
     <div>
       <Title greeting={ greeting }/>
+      <ItemCount initial={1} stock={5} onAdd={onAdd}/>
     </div>
   )
 }
